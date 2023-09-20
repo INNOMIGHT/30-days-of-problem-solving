4e872359f889baaca8d53b4a989c9cd4178b0a6c
Deepanshi Sharma <deepanshisharma2510@gmail.com>
2023-09-20 15:18:08 +0530

    day 4

---
+++ deepanshi_30_days/pascals-triangle.py
@@ -0,0 +1,11 @@
+def generate(self, numRows):
+        res = [[1]]
+        
+        for i in range(1,numRows):
+            temp = [0] + res[i-1]  + [0]
+            row = []
+            for j in range(len(temp)-1):
+                row.append(temp[j] + temp[j+1])
+           
+            res.append(row)
+        return res
